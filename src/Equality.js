585669c359d9aa25972a8cfb406136e84c9d34ab
simtor <simone.ram@ada.ac.uk>
2020-07-21 14:00:56 +0100

    first commit

---
+++ src/Equality.js
@@ -0,0 +1,30 @@
+
+
+console.log("Equality (==)");
+
+console.log(1 == 1);
+// expected output: true
+
+console.log('hello' == 'hello');
+// expected output: true
+
+console.log('1' ==  1);
+// expected output: true
+
+console.log(0 == false);
+// expected output: true
+
+
+console.log("Strict equality (===)");
+
+console.log(1 === 1);
+// expected output: true
+
+console.log('hello' === 'hello');
+// expected output: true
+
+console.log('1' ===  1);
+// expected output: false
+
+console.log(0 === false);
+// expected output: false
