b7ed2432ca0aa95470df24be2cad3a09e415d1f0
simtor <simone.ram@ada.ac.uk>
2020-08-18 09:30:23 +0100

    Added files

--- src/closures.js
+++ src/closures.js
@@ -1,43 +1,39 @@
 //Closures is a way to  invoke a function. Thge the function is invoked a new scope is create for that call.
 
-
-function buildName(name) { 
-    var greeting = "Hello, " + name + "!"; 
-    var sayName = function() {
-        var welcome = greeting + " Welcome!";
-        console.log(greeting); 
-    };
-    return sayName; 
-}
-
-var sayMyName = buildName("Simone");
-sayMyName();  // Hello, Simone. Welcome!
-sayMyName();  // Hello, Simone. Welcome!
-sayMyName();  // Hello, Simone. Welcome!
-
-
-
-
-function buildContor(i) { 
-    var contor = i;
-    var displayContor = function() {
-        console.log(contor++);
-        contor++;
-    };
-    return displayContor; 
-}
-
-var myContor = buildContor(1);
-myContor(); // 1
-myContor(); // 2
-myContor(); // 3
-
-// new closure - new outer scope - new contor variable
-var myOtherContor = buildContor(10);
-myOtherContor(); // 10 
-myOtherContor(); // 11
-
-// myContor was not affected 
-myContor(); // 4
+function buildName(name) {
+    var greeting = "Hello, " + name + "!"
+    var sayName = function () {
+      var welcome = greeting + " Welcome!"
+      console.log(greeting)
+    }
+    return sayName
+  }
+  
+  var sayMyName = buildName("Simone")
+  sayMyName() // Hello, Simone. Welcome!
+  sayMyName() // Hello, Simone. Welcome!
+  sayMyName() // Hello, Simone. Welcome!
+  
+  function buildCounter(i) {
+    var contor = i
+    var displayCounter = function () {
+      console.log(contor++)
+      contor++
+    }
+    return displayCounter
+  }
+  
+  var myCounter = buildCounter(1)
+  myCounter() // 1
+  myCounter() // 3
+  myCounter() // 5
+  
+  // new closure - new outer scope - new contor variable
+  var myOtherCounter = buildCounter(10)
+  myOtherCounter() // 10
+  myOtherCounter() // 12
+  
+  // myContor was not affected
+  myCounter() // 7
 
 
