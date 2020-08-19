0f34e078a881ddff6e7103c2a9ba53f906f56524
simtor <simone.ram@ada.ac.uk>
2020-08-19 11:29:35 +0100

    Added comments

--- src/ES6/Modules.js
+++ src/ES6/Modules.js
@@ -4,7 +4,10 @@
 
 // exports.getName = getName;
 
+//↑ This example will work because it use common JS
 
+
+// ↓ This example will  not work if you use common JS so you need to  install typescript
 
 export default class User { // just add "default"
   constructor(name) {
--- src/ES6/Modules2.js
+++ src/ES6/Modules2.js
@@ -2,7 +2,11 @@
 
 // console.log(`User: ${user.getName()}`);
 
+//↑ This example will work because it use common JS
+
+
+// ↓ This example will  not work if you use common JS so you need to  install typescript
 
 import User from './Modules.js'; // not {User}, just User
 
-new User('John');
+new User('John');//<------This ex
