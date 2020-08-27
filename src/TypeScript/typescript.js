b4c028172f13f5e395536e0a3f1071a0a2a553f7
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-08-27 12:25:33 +0100

    typecript example

---
+++ src/TypeScript/typescript.js
@@ -0,0 +1,7 @@
+var myName = function (person) {
+    return "Hello, " + person;
+};
+//myName function that expects an arugument as a String
+var user = "Harman Kalair";
+console.log(myName(user));
+//# sourceMappingURL=typescript.js.map
---
+++ src/TypeScript/typescript.ts
@@ -0,0 +1,7 @@
+const myName = (person: string) => {
+  return "Hello, " + person;
+};
+//myName function that expects an arugument as a String
+
+let user = "Harman Kalair";
+console.log(myName(user));
