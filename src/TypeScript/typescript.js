915dc246fcf8147e03334c4cec5851eeb93f6fd2
harmankalair <56550789+harmankalair123@users.noreply.github.com>
2020-09-01 14:56:46 +0100

    added files to git-ignore

--- src/TypeScript/typescript.js
+++
@@ -1,7 +0,0 @@
-//myName function that expects an argument as a String
-var myName = function (person) {
-    return "Hello, " + person;
-};
-var user = "Harman Kalair";
-console.log(myName(user));
-//# sourceMappingURL=typescript.js.map
